fcf39824bb266b900015daa7cedf89a3757e1982
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-04-29 00:59:26 +0500

    add redux files

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import AppRoutes from "./routes/Routes";
 function App() {
   return (
+    <Provider store={store}>
       <Router>
         <AppRoutes />
       </Router>
-
+    </Provider>
   );
 }
 
