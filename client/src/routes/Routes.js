074dd04570914595acd1c0099541e5f66df85f6c
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-04-29 00:42:20 +0500

    add tailwind css

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route} from "react-router-dom";
 // import "./route.css";
 import Home from "../pages/Home.js";
+import Login from "../login-model/Login.js";
 
 function AppRoutes() {
 
@@ -10,6 +11,7 @@ function AppRoutes() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
