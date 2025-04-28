3304768a77c5469dc8f764966033dd5329ecd1a4
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-04-29 01:07:02 +0500

    create server

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+// Load environment variables from config.env
+dotenv.config({ path: './config.env' });
+
+// Routes
+// const userRoutes = require('./routes/user.js');
+
+// Express
+const app = express();
+app.use(express.json());
+
+// Cors
+app.use(cors());
+
+// Connection to MongoDB
+// require('./db/connection.js');
+
+// Load Routes
+// app.use('/api/user', userRoutes);
+
+// For Server Port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running at port http://localhost:${PORT}`);
+});
+
+// For Testing
+app.post('/createblog', (req, res) => {
+  res.send('API is working');
+});
+
+app.get('/', (req, res) => {
+  res.send('API is working');
+});
+
